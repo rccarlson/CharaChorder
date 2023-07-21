ffc24601286c4e3600d1080b0d2f44cb65687b35
Riley Carlson <riley.carlson@ksb.com>
2023-07-21 14:36:09 -0400

    Build basic implementation for discovering and choosing a serial device

--- CharaChorderInterface/CharaChorder.cs
+++ CharaChorderInterface/CharaChorder.cs
@@ -2,20 +2,45 @@
 
 namespace CharaChorder;
 
-public class CharaChorder
+public class CharaChorder : IDisposable
 {
-	const int BaudRate = 115200;
+	const int BaudRate = 115_200;
+	const int VendorID = 0x239a;
 
-	public static CharaChorder FromSerial()
+	private SerialPort? _port = null;
+
+	public static (string? PortName, string? Description)[] GetSerialPorts()
+		=> Utility.SerialUtility.GetAllCOMPorts()
+			.Select(port => (port.name, port.bus_description))
+			.ToArray();
+
+	private CharaChorder() { }
+
+	/// <summary>
+	/// Build a <see cref="CharaChorder"/> from 
+	/// </summary>
+	/// <param name="serialPortName"></param>
+	/// <returns></returns>
+	public static CharaChorder? FromSerial(string serialPortName)
 	{
-		var all = SerialUtility.GetAllCOMPorts();
-		var portNames = SerialPort.GetPortNames();
-		
-		foreach(var portName in portNames)
+		var cc = new CharaChorder()
 		{
-			var port = new SerialPort(portName, baudRate: BaudRate);
-		}
+			_port = new SerialPort(serialPortName, baudRate: BaudRate)
+		};
+		try { cc._port.Open(); }
+		catch (UnauthorizedAccessException ex) { ThrowUnauthorized(ex); }
+		if (!cc._port.IsOpen) ThrowUnauthorized();
+		return cc;
 
-		return default;
+		void ThrowUnauthorized(UnauthorizedAccessException? ex = null)
+		{
+			cc.Dispose();
+			throw new UnauthorizedAccessException($"Unable to open port {serialPortName}. Is it open elsewhere?", ex);
+		}
+	}
+
+	public void Dispose()
+	{
+		_port?.Dispose();
 	}
 }
