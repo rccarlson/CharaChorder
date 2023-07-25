8b0f7f79ad58f62d6157f0fcb942c3aba1c553a8
Riley Carlson <riley.carlson@ksb.com>
2023-07-25 16:38:22 -0400

    Add test project

--- DebuggerProject/Program.cs
+++ DebuggerProject/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace CharaChorder;
+﻿namespace CharaChorderInterface;
 
 internal class Program
 {
 	static void Main(string[] args)
 	{
-		CharaChorder.FromSerial();
+		var actionmap = Maps.ActionMap;
+		var ports = CharaChorder.GetSerialPorts();
+		using var cc = CharaChorder.FromSerial("COM3");
+		var chordCount = cc?.GetChordmapCount();
+		var id = cc?.GetID();
+		var version = cc?.GetVersion();
+		var chord = cc?.GetChordmapByIndex(23);
+		var chordByHex = cc?.GetChordmapByChord(chord.HexChord);
+
+		Thread.Sleep(1_000);
 	}
 }
