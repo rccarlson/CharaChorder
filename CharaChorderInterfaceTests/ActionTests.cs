a9947b1302394047b5aaa525592c8a023e8278eb
Riley Carlson <riley.carlson@ksb.com>
2023-08-02 14:03:36 -0400

    Add tests for Action map

---
+++ CharaChorderInterfaceTests/ActionTests.cs
@@ -0,0 +1,15 @@
+﻿using CharaChorderInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharaChorderInterfaceTests;
+
+public class ActionTests
+{
+	[Test] public void HasActions() => Assert.That(Maps.ActionMap, Is.Not.Empty);
+
+	[Test] public void MaxActionLengthAssumption() => Assert.That(Maps.ActionMap.Max(action => action.Length), Is.LessThanOrEqualTo(13));
+}
