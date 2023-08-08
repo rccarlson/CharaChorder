3471b0eb0f23bd4e9b1cb40c59489fc7ec56e4e5
Riley Carlson <rileyccarlson97@gmail.com>
2023-08-08 08:18:33 -0400

    scratch work for finding maximum phrase side

--- DebuggerProject/Program.cs
+++ DebuggerProject/Program.cs
@@ -11,6 +11,31 @@ internal class Program
 		cc.LoggingAction = Console.WriteLine;
 		cc.Open();
 
-		var beeMovieChord = Chordmap.FromAscii(new string[] { "b", "e", "t" }, TheBeeMovie);
+		//var smallChord = Chordmap.FromAscii(new[] { 's', 'h', 'r' }, "Shrek 2");
+		////cc.SetChordmap(smallChord);
+		//cc.DeleteChordmap(smallChord);
+
+		//return;
+
+
+// shrek limit: 124 actions, 256 long hex
+// lorem ipsum: 125 actions, 250 long hex
+		var shrek = CompressChord(new string[] { "s", "h", "r" }, File.ReadAllText(@"C:\Users\Riley\Downloads\Shrek.txt"), 125);
+		//cc.SetChordmap(shrek);
+
+		var loremIpsum = CompressChord(new string[] { "l", "i", "p" }, LoremIpsum, 126);
+
+		//cc.SetChordmap(loremIpsumChord);
 	}
+
+	static Chordmap CompressChord(IEnumerable<string> chordActions, string phrase, int phraseActionCount)
+	{
+		var largeChord = Chordmap.FromAscii(chordActions, phrase);
+		var compressedChord = Chordmap.FromActions(largeChord.ChordActions, largeChord.PhraseActions.Take(phraseActionCount));
+		return compressedChord;
+	}
+
+	const string LoremIpsum = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+""";
 }
